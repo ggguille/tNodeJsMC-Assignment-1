e9a61e2098283b4a0e7c501bcaa5f1d971bbefa2
Guille <ggguille@live.com>
2018-10-14 21:29:15 +0200

    create and instantiate /hello request handler

--- index.js
+++ index.js
@@ -7,10 +7,19 @@
 // Dependencies
 const fs = require('fs');
 const config = require('./lib/config');
-const HttpServer = require('./lib/httpServer');
+const HttpServer = require('./lib/server/httpServer');
+const HandlerManager = require('./lib/server/httpRequestHandler').HandlerManager;
+const HelloHandler = require('./lib/handlers/helloHandler');
+
+// Instantiate HTTP request handler manager
+const handlerManager = new HandlerManager();
+// Add /hello request handler
+handlerManager.addHandler(new HelloHandler());
 
 // Instantiate HTTP server
-const httpServer = new HttpServer.Builder('http').build();
+const httpServer = new HttpServer.Builder('http')
+    .setHandlerManager(handlerManager)
+    .build();
 // Start HTTP server
 httpServer.listen(config.httpPort);
 
@@ -19,6 +28,7 @@ const sslKey = fs.readFileSync('./ssl/key.pem');
 const sslCert = fs.readFileSync('./ssl/cert.pem');
 const httpsServer = new HttpServer.Builder('https')
     .addSSL(sslKey, sslCert)
+    .setHandlerManager(handlerManager)
     .build();
 // Start HTTPS server
 httpsServer.listen(config.httpsPort);
